c63a573139a55154b2a396e6c0f2bdd5b20b51eb
unknown <niambaust17@gmail.com>
2021-02-07 15:39:03 +0600

    add arraow function and foreach loop

--- script.js
+++ script.js
@@ -2,46 +2,48 @@ const searchBtn = document.getElementById('search');
 const mealSearch = document.getElementById('meal-search');
 const mealContainer = document.getElementById('meal-container');
 const mealDetail = document.getElementById('meal-full-detail');
-searchBtn.addEventListener('click', function ()
+
+searchBtn.addEventListener('click', () =>
 {
-    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${ mealSearch.value }`)
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${ mealSearch.value }`)
         .then(res => res.json())
         .then(data => showMatchingMeal(data))
-        .catch(error => alert('Give First Letter Only'))
+        .catch(error => alert('No Food Found'))
 
     const showMatchingMeal = data =>
     {
-        for (meal of data.meals)
+        data.meals.forEach(meal =>
         {
             const mealImgSrc = meal.strMealThumb;
             const mealName = meal.strMeal;
             const mealId = meal.idMeal;
-            // console.log(mealId);
+
             const mealDiv = document.createElement('div');
             mealDiv.className = 'meal';
 
             const mealInfo = `
             <img onclick="getMealInfo('${ mealId }')" src="${ mealImgSrc }" alt="">
-            <p>${ mealName }</p>
+            <p onclick="getMealInfo('${ mealId }')">${ mealName }</p>
             `;
+
             mealDiv.innerHTML = mealInfo;
             mealContainer.appendChild(mealDiv);
-            // console.log(meal);
-            // console.log(meal.strMeal);
-        }
-        // console.log(data.meals[1].strMeal)
+        });
     }
     mealSearch.value = '';
+    mealContainer.innerHTML = '';
+    mealDetail.innerHTML = '';
 });
+
 
 const getMealInfo = mealId =>
 {
-    console.log(mealId);
     fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${ mealId }`)
         .then(res => res.json())
-        // console.log(data.meals[0].strMeal)
         .then(data => showMealInfo(data.meals[0]));
 }
+
+
 const showMealInfo = meal =>
 {
     mealDetailInfo = `
@@ -49,17 +51,24 @@ const showMealInfo = meal =>
         <div class='info'>
             <h5>${ meal.strMeal }</h5>
             <p>Ingredients</p>
-            <ol id="ingredients">
-                <li>${ meal.strMeasure1 } ${ meal.strIngredient1 }</li>
-                <li>${ meal.strMeasure2 } ${ meal.strIngredient2 }</li>
-                <li>${ meal.strMeasure3 } ${ meal.strIngredient3 }</li>
-                <li>${ meal.strMeasure4 } ${ meal.strIngredient4 }</li>
-                <li>${ meal.strMeasure5 } ${ meal.strIngredient5 }</li>
-                <li>${ meal.strMeasure6 } ${ meal.strIngredient6 }</li>
-                <li>${ meal.strMeasure7 } ${ meal.strIngredient7 }</li>
-                <li>${ meal.strMeasure8 } ${ meal.strIngredient8 }</li>
-            </ol>
+            <ul id="ingredients">
+                
+            </ul>
         </div>
     `;
+
     mealDetail.innerHTML = mealDetailInfo;
+
+    for (let i = 1; i < 21; i++)
+    {
+        let li = document.createElement('li');
+        let measure = 'strMeasure' + i;
+        let ingredient = 'strIngredient' + i;
+        if (meal[ingredient] !== '' && meal[ingredient] !== null)
+        {
+            const ingredientInfo = meal[measure] + '  ' + meal[ingredient];
+            li.innerText = ingredientInfo;
+            document.getElementById('ingredients').appendChild(li);
+        }
+    }
 }
